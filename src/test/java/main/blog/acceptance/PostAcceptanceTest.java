dbb435950be0913d2c4195f48178a6c5f031f840
GuDonghee <dev.donghee1995@gmail.com>
2024-01-07 17:00:55 +0900

    feat: 포스트 등록 API 요청 시, 인가검증 기능 생성

--- src/test/java/main/blog/acceptance/PostAcceptanceTest.java
+++ src/test/java/main/blog/acceptance/PostAcceptanceTest.java
@@ -1,10 +1,12 @@
 package main.blog.acceptance;
 
-import main.DatabaseCleaner;
-import main.blog.controller.dto.PostCreateRequest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import main.DatabaseCleaner;
+import main.auth.controller.dto.LoginRequest;
+import main.blog.controller.dto.PostCreateRequest;
+import main.blog.controller.dto.UserCreateRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,10 +28,29 @@ public class PostAcceptanceTest {
     @Autowired
     private DatabaseCleaner databaseCleaner;
 
+    private String accessToken;
+
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
         databaseCleaner.execute();
+
+        UserCreateRequest userCreateRequest = new UserCreateRequest("데이빗", "koo@koo.com", "test1234!@");
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(userCreateRequest)
+                .when().post("/users")
+                .then().log().all();
+
+        LoginRequest loginRequest = new LoginRequest("koo@koo.com", "test1234!@");
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(loginRequest)
+                .when().get("/auth")
+                .then().log().all()
+                .extract();
+
+        accessToken = (String) response.as(Map.class).get("token");
     }
 
     @DisplayName("게시물을 등록하면 상태코드 200을 응답한다.")
@@ -39,6 +62,7 @@ public class PostAcceptanceTest {
         // when
         ExtractableResponse<Response> response = RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header("Authorization", "Bearer " + accessToken)
                 .body(request)
                 .when().post("/post")
                 .then().log().all()
