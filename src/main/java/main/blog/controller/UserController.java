0f5582a54a41a12ed97ad2b09af83d403d2b9540
GuDonghee <dev.donghee1995@gmail.com>
2024-02-11 19:41:23 +0900

    chore: 어플리케이션 서버 CD 파이프라인을 현재 인프라에 맞게 수정

--- src/main/java/main/blog/controller/UserController.java
+++ src/main/java/main/blog/controller/UserController.java
@@ -1,15 +1,17 @@
 package main.blog.controller;
 
+import jakarta.validation.Valid;
 import main.blog.controller.dto.UserCreateRequest;
 import main.blog.service.UserService;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
 import java.net.URI;
+import java.net.UnknownHostException;
 
 @RestController
 @RequestMapping("/users")
@@ -24,6 +26,13 @@ public class UserController {
     @PostMapping
     public ResponseEntity<Void> signUp(@RequestBody @Valid UserCreateRequest request) {
         Long userId = this.userService.signUp(request);
-        return ResponseEntity.created(URI.create("/users/" + userId)).build();
+        InetAddress local = null;
+        try {
+            local = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+        String ip = local.getHostAddress();
+        return ResponseEntity.created(URI.create("/users/" + userId + ":" + ip)).build();
     }
 }
